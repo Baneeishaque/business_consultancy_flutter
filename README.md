7eb84cacfaaec733936521c83cbde4fd00acfc35
Banee Ishaque K <Baneeishaque@gmail.com>
2020-05-03 19:17:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# business_consultancy
+ 
---
+++ android/app/src/main/java/ndk/business_consultancy/business_consultancy/MainActivity.java
@@ -0,0 +1,6 @@
+package ndk.business_consultancy.business_consultancy;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
